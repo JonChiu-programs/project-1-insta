01943f3e2d06bced4d162d9de2f185552c8a4478
JonChiu-programs <jonwchiu@gmail.com>
2026-03-18 11:43:43 -0400

    Got the foxes to fox but they don't appear on their own slides; need to fix this.

--- project-1-insta.js
+++ project-1-insta.js
@@ -58,6 +58,34 @@ export class Project1Insta extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--play-list-project-label-font-size, var(--ddd-font-size-s));
       }
+
+      insta-indicator{
+          transform: translateX(200px);
+        }
+
+        insta-card{
+      display: block;
+        /* outline: auto; I'm just keeping this line as I found it by accident and I found the results to be fascinating despite not being what I was going for. */ 
+        transform: translateY(-70px);
+        width: 1000px;
+        height: 800px;
+        border: var(--ddd-border-md);
+        text-align: center;
+        font-size: 100px;
+        overflow-y: scroll;
+        }
+
+
+    h3{
+      text-align: center;
+      position: flex;
+    }
+
+    hr{
+      width: 75px;
+      color: white;
+      padding-bottom: 20px;
+    }
         
       @media (min-width: 500px) and (max-width: 800px){
         insta-indicator{
@@ -76,7 +104,12 @@ export class Project1Insta extends DDDSuper(I18NMixin(LitElement)) {
   <insta-arrow
     @previous="${this.previous}"
     @next="${this.next}">
-    <slot></slot>
+    <insta-card>
+      <h3>Hello</h3>
+        <hr>
+        <p>Look at all these foxes!</p>
+        <slot></slot>
+    </insta-card>
     <insta-indicator
     @insta-index-changed="${this.handleEvent}"
     .total="${this.slides ? this.slides.length : 0}"
@@ -91,6 +124,7 @@ export class Project1Insta extends DDDSuper(I18NMixin(LitElement)) {
   this.slides = Array.from(this.querySelectorAll("insta-slide"));
   this.total = this.slides.length; //This line of code is adapted from bpark5 in order to get indicator dots to appear without needing user input
   this.changeSlide();
+  this.getFox(); //Need to figure out how to prevent duplicate images on a slide that already has one.
   }
   
   changeSlide() {
@@ -112,7 +146,8 @@ next() {
   if (this.currentIndex < this.slides.length - 1) {
       this.currentIndex++;
       this.changeSlide();
-  }
+      this.getFox(); //For when I need a laugh: the reason why this line wasn't working was because I put a "," instead of a "." on accident lol.
+    }
 }
 
 previous() {
@@ -127,6 +162,27 @@ handleEvent(e){
   this.changeSlide();
 }
 
+getFox() { //Adapted from btopro example; comments are kept in to remind me what everything is supposed to do.
+      fetch("https://randomfox.ca/floof/").then((resp) => {
+      // headers indicating the request was good, then process it
+      if (resp.ok) {
+        // return the response as JSON. .text() is another valid response
+        // though that's more useful for HTML / non data object responses
+        return resp.json();
+      }
+    })
+    .then((data) => {
+        // THEN after the 2nd promise resolves, do this
+        // the data being passed in, whill be the response object as json()
+        // from the previous Promise resolving
+        // here we can see that data.image allows us to access the image
+        // attribute in the response
+        let image = document.createElement('img');
+        image.src = data.image;
+        image.loading = "lazy";
+        this.appendChild(image);
+    });
+  }
 }
 
 globalThis.customElements.define(Project1Insta.tag, Project1Insta);
